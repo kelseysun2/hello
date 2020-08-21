1816b54e4d1f051656168769c38b663142d1a334
Kelsey Sunderman <ksunderman2@huskers.unl.edu>
2020-08-21 01:20:30 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kelsey Sunderman
+ *         ksunderman2@huskers.unl.edu
+ * Date: 2020/08/19
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kelsey Sunderman!\n");
+
+  return 0;
+}
