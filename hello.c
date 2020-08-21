c0a3a5bc9323f9a0999e7e73f18163a912ccdeb4
Jeremiah Reno <jreno3@huskers.unl.edu>
2020-08-21 21:45:22 +0000

    kelsey

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Kelsey Sunderman!\n");
   printf("Computer Science\n");
+  printf("Jeremiah Reno!\n");
+  printf("Computer Engineering\n");
 
   return 0;
 }
